3235ab4c00e1834c2f24f4e070cfbde7710c052b
erinbedri <erinbedri@gmail.com>
2022-06-17 12:29:18 +0200

    solved exercise 2 from lab

---
+++ labs/2-asynchronous-programming/02. JS-Applications-Asynchronous-Programming-Lab-Resources/02.Github-Repos/scripts/app.js
@@ -0,0 +1,31 @@
+async function loadRepos() {
+
+	const username = document.getElementById('username').value;
+
+	const url = `https://api.github.com/users/${username}/repos`;
+
+	let list = document.getElementById('repos');
+	list.innerHTML = '';
+
+	try {
+		const res = await fetch(url);
+
+		if (res.status != 200) {
+			throw new Error('Incorrect username')
+		}
+
+		const data = await res.json();
+
+		data.forEach(repo => {
+			let li = document.createElement('li');
+			let a = document.createElement('a');
+			a.href = repo.html_url;
+			a.textContent = repo.full_name;
+			li.appendChild(a);
+			list.appendChild(li);
+		});
+
+	} catch (error) {
+		list.textContent =  error;
+	}
+}
