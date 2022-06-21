529851b26de1cdf79d23f6b84697c544b69af0f2
erinbedri <erinbedri@gmail.com>
2022-06-21 12:56:27 +0200

    solved first exercise of homework

--- exercises/3-remote-data-and-authentication/03. JS-Applications-Data-and-Authentication-Exercise/01.Messenger/app.js
+++ exercises/3-remote-data-and-authentication/03. JS-Applications-Data-and-Authentication-Exercise/01.Messenger/app.js
@@ -1,5 +1,48 @@
 function attachEvents() {
-    console.log('TODO...');
+    const sendButton = document.getElementById('submit');
+    const refreshButton = document.getElementById('refresh');
+    
+    let messageArea = document.getElementById('messages');
+
+    refreshButton.addEventListener('click', () => {
+        messageArea.textContent = '';
+
+        let url = 'http://localhost:3030/jsonstore/messenger';
+
+        fetch(url)
+            .then(res => res.json())
+            .then(messages => {
+                Object.values(messages).forEach(message => {
+                    messageArea.textContent += `${message.author}: ${message.content}\n`;
+                })
+            })
+    })
+
+    sendButton.addEventListener('click', () => {
+        let authorElement = document.querySelector('[name="author"]');
+        let messageElement = document.querySelector('[name="content"]');
+
+        let url = 'http://localhost:3030/jsonstore/messenger';
+
+        let content = {
+            author: authorElement.value,
+            content: messageElement.value,
+        }
+
+        fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(content)
+        })
+            .then(res => res.json())
+            .then(message => {
+                console.log(message)
+                messageArea.textContent += `${message.author}: ${message.content}\n`;
+            })
+    })
 }
 
+
 attachEvents();
+
+
