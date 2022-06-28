64febdda53b220a120aebbbb1bdaed9d87bd7400
erinbedri <erinbedri@gmail.com>
2022-06-28 14:04:57 +0200

    basic structure done

--- exercises/4-single-page-applications/04. JS-Applications-Single-Page-Applications-Exercise/02.Movies/src/app.js
+++ exercises/4-single-page-applications/04. JS-Applications-Single-Page-Applications-Exercise/02.Movies/src/app.js
@@ -2,6 +2,7 @@ import {registerPage} from './register.js';
 import {loginPage} from './login.js';
 import {homePage} from './home.js';
 import {createPage} from './create.js';
+import {updateNav} from './util.js';
 
 document.querySelector('nav').addEventListener('click', onNavigate);
 document.querySelector('#add-movie-button a').addEventListener('click', onNavigate);
@@ -17,18 +18,21 @@ const routes = {
 function onNavigate(event) {
     if (event.target.tagName == 'A' && event.target.href) {
         event.preventDefault();
-
+        
         const url = new URL(event.target.href);
-        const view = routes[url];
+        const view = routes[url.pathname];
 
         if (typeof view == 'function') {
+            console.log(event.target)
             view();
         }
     }
 }
 
 function logout() {
-    console.log('logging out')
+    console.log('logging out');
+    updateNav();
 }
 
-homePage()
+updateNav();
+homePage();
