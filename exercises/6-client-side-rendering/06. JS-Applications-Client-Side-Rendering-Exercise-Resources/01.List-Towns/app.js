750c4c95f5d21b014d43ff249bfa6ef56b212744
erinbedri <erinbedri@gmail.com>
2022-07-06 16:48:07 +0200

    set up and solved 1st exercise

---
+++ exercises/6-client-side-rendering/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/01.List-Towns/app.js
@@ -0,0 +1,26 @@
+import { render, html } from '../../../../node_modules/lit-html/lit-html.js';
+
+const loadBtn = document.getElementById('btnLoadTowns');
+loadBtn.addEventListener('click', onClick);
+
+const template = (data) => html`
+        <ul>
+            ${data.map(city => html`
+            <li>
+                ${city}
+            </li>`)}
+        </ul>
+`
+
+function onClick(e) {
+    let root = document.getElementById('root');
+    let inputField = document.getElementById('towns');
+
+    if (inputField.value != '') {
+        e.preventDefault()
+
+        render(template(inputField.value.split(', ')), root)
+
+        inputField.value = '';
+    }
+}
