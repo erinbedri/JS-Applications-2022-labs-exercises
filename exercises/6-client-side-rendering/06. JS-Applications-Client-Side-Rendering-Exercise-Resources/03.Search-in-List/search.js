1e22e364570afcb898c8c6cf84577fb372f4512c
erinbedri <erinbedri@gmail.com>
2022-07-06 20:29:10 +0200

    started 3rd

--- exercises/6-client-side-rendering/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
+++ exercises/6-client-side-rendering/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
@@ -1,3 +1,65 @@
+import { render, html } from '../../../../node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+
+let body = document.body;
+let number;
+
+let initialState = (data) => html`
+      <article>
+         <div id="towns">
+            <ul>${
+               data.map(city => html`<li>${city}</li>`)
+            }</ul>
+         </div>
+         <input type="text" id="searchText" />
+         <button @click="${search}">Search</button>
+         <div id="result"></div>
+      </article>
+`
+
+render(initialState(towns), body);
+
+let templateSearch = (data) => html`
+      <article>
+         <div id="towns">
+            <ul>${
+               data.map(city => html`<li>${city}</li>`)
+            }</ul>
+         </div>
+         <input type="text" id="searchText" />
+         <button @click="${search}">Search</button>
+         <div id="result">${number} matches found</div>
+      </article>
+`
+
 function search() {
-   // TODO
+   let searchElement = document.getElementById('searchText');
+   let number = 5
+   
+   render(templateSearch(towns), body);
+
+   searchElement.value = '';
 }
+
+/*
+const townsList = document.getElementById('towns');
+const searchBtn = document.querySelector('button');
+
+searchBtn.addEventListener('click', search);
+
+let template = (data) => html`
+        <ul>${
+            data.map(city => html`<li>${city}</li>`)
+        }</ul>
+`
+render(template(towns), townsList);
+
+function search() {
+   let searchElement = document.getElementById('searchText');
+   let matchesElement = document.getElementById('result');
+
+   console.log(searchElement.value)
+
+   searchElement.value = '';
+}
+*/
