2d917146c9cfbcd8c18cd397b8b02e7c6f6cda0f
erinbedri <erinbedri@gmail.com>
2022-07-08 14:39:29 +0200

    solved 4th

--- exercises/6-client-side-rendering/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/04.Fill-Dropdown/dropdown.js
+++ exercises/6-client-side-rendering/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,45 @@
-function addItem() {
-    console.log('TODO:...');
-}
+import { html, render } from '../../../../node_modules/lit-html/lit-html.js';
+
+const url = 'http://localhost:3030/jsonstore/advanced/dropdown';
+
+async function getTowns() {
+    const response = await fetch(url);
+    return await response.json();
+};
+
+let template = (data) => html`
+            <select id="menu">
+                ${data.map(city => html`<option value=${city._id}>${city.text}</option>`)}
+            </select>
+`
+
+let menu = document.querySelector('div');
+let towns = Object.values(await getTowns());
+
+function update(towns) {
+    let result = template(towns);
+    render(result, menu);
+};
+
+update(towns);
+
+document.querySelector('form').addEventListener('submit', addItem);
+
+async function addItem(e) {
+    e.preventDefault();
+
+    let text = document.getElementById('itemText').value;
+
+    if (text != '') {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ text })
+        })
+
+        towns.push(await response.json());
+        update(towns);
+
+        document.getElementById('itemText').value = '';
+    }
+};
